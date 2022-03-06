aa82fa2d9d0a4c00463cf852a6affc5818e4c8c2
joabehenrique <henriquecidoz@hotmail.com>
2022-03-06 14:51:44 -0300

    feat: created findAll, update, findById and delete of employee

--- backend/src/main/java/com/amigoscode/employeemanager/service/EmployeeService.java
+++ backend/src/main/java/com/amigoscode/employeemanager/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package com.amigoscode.employeemanager.service;
 
 import com.amigoscode.employeemanager.entity.Employee;
+import com.amigoscode.employeemanager.exception.EmployeeNotFoundException;
 import com.amigoscode.employeemanager.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,5 +19,22 @@ public class EmployeeService {
     public Employee saveEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee by id "+id+" not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepository.deleleEmployeeById(id);
     }
 }
