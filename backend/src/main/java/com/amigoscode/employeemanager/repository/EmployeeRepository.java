6d019aad9c484dd23da023c5950ca787896caeea
joabehenrique <henriquecidoz@hotmail.com>
2022-03-06 14:26:27 -0300

    feat: created employee repository

---
+++ backend/src/main/java/com/amigoscode/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.amigoscode.employeemanager.repository;
+
+import com.amigoscode.employeemanager.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
