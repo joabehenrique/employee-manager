567c80089baac0c39a4d536b3ea3da6e370bb4a1
joabehenrique <henriquecidoz@hotmail.com>
2022-03-06 14:53:38 -0300

    feat: created exception for employee not found

---
+++ backend/src/main/java/com/amigoscode/employeemanager/exception/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.amigoscode.employeemanager.exception;
+
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
