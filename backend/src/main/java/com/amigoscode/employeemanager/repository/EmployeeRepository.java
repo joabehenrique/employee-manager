0714abd60578d882280b553436a4f28a247100de
joabehenrique <henriquecidoz@hotmail.com>
2022-03-06 20:13:52 -0300

    fix: removed custom methods

--- backend/src/main/java/com/amigoscode/employeemanager/repository/EmployeeRepository.java
+++ backend/src/main/java/com/amigoscode/employeemanager/repository/EmployeeRepository.java
@@ -4,10 +4,5 @@ import com.amigoscode.employeemanager.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    void deleleEmployeeById(Long id);
-
-    Optional<Employee> findEmployeeById(Long id);
-}
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> { }
